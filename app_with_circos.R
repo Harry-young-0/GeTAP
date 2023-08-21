b3e733ae3d4f2a4131e3e2f212fd69f96aacf226
Hy14913 <66262215+Hy14913@users.noreply.github.com>
2023-08-21 10:29:40 +0100

    Add files via upload
    up to date pipeline and shiny app

--- app_with_circos.R
+++ app_with_circos.R
@@ -3,25 +3,29 @@ library(tidyverse)
 library(plotly)
 library(bslib)
 library(circlize)
+
 data_list = list(
-  "SNP Count per Tissue Group" =SNPcountperTissuegroup,
-  "SNP by Trait Group" =SNP_by_traitgroupe0,
-  "Logp*Direction x traitgroup" =pvalvstraitgroupe1,
-  "Logp*Direction x traitgroup (No Other or Other gene Expression Change)" = pvalvstraitgroupNO_O_e2_leg,
-  "Genomic Regions of these SNP-Trait Associations"=snpvsregion,
-  "Genomic Diagram: SNPs Relative to Gene and Region" =gene_structure_RED+theme(legend.position = "top"))
+  "A1 SNP Count per Tissue Group (Raw GTEx)" =A1SNPcountperTissuegroup,
+  "A2 SNP Count per Tissue Group (after LD and PheWAS)" = A2SNPcountperTissuegroup,
+  "A3 STA Count per Tissue Group" = A3STAcountperTissuegroup,
+  "A4 Genomic Regions of these SNP-Trait Associations" = snpvsregion,
+  "B r2 linear regression" = BCorrelation_tg,
+  "B2 r2 linear regression, zoomed" = B2,
+  "C SNP vs Number of Tissues Recorded by GTEx" =CPosition_count_OVERVIEW,
+  "D SNP by Trait Group" =DSNP_by_traitgroup,
+  "D2 SNP by Trait Group Relative to Full Database" =D2RELSNP_by_traitgroup,
+  "E1 Number of STAs per Trait Group / Total number of STAs" =E1,
+  "E2 E1 (without Other of Other Gene Expression Change)" = E2,
+  "F1 Logp*Direction vs Trait Group" =F1pvalvstraitgroup,
+  "F2 Logp*Direction vs Trait Group (No Other or Other Gene Expression Change)" = F2pvalvstraitgroupNO_O,
+  "Genomic Diagram: SNPs Relative to Gene and Region (After LD and PheWAS)" =gene_structure_RED+theme(legend.position = "top"))
 circos_choice =list("Position to Tissue Group" =test,
                       "Tissue Group to Trait Group" = test2,
                       "Position to Traitgroup" = test3)
 grid_col_choice = list("Position to Tissue Group" =grid_col_tissue,
                        "Tissue Group to Trait Group" = grid_col_trait,
                        "Position to Traitgroup" = grid_col_trait)
-Top5_choice = list("Number 1 Trait Group" =e4_1,
-                   "Number 2 Trait Group" =e4_2,
-                   "Number 3 Trait Group" =e4_3,
-                   "Number 4 Trait Group" =e4_4,
-                   "Number 5 Trait Group" =e4_5,
-                   "Other Gene Expression Change" = OGECe3)
+Top5_choice = Top5_choice
 
 ui = navbarPage(
   
@@ -36,17 +40,13 @@ ui = navbarPage(
         shiny::selectInput(
           inputId = "dataset_choice",
           label = "Plot choice",
-          choices =c("SNP Count per Tissue Group",
-                     "SNP by Trait Group",
-                     "Logp*Direction x traitgroup",
-                     "Logp*Direction x traitgroup (No Other or Other gene Expression Change)",
-                     "Genomic Regions of these SNP-Trait Associations",
-                     "Genomic Diagram: SNPs Relative to Gene and Region")
+          choices =names(data_list)
           
         )
       ),
       mainPanel(
-        tabPanel("Plot",plotOutput("my_plot", width=700, height=500))
+        tabPanel("Plot",plotOutput("my_plot",
+                                   width=800, height=600))
         )
       )
     ),
@@ -59,20 +59,19 @@ ui = navbarPage(
         shiny::selectInput(
           inputId = "dataset_circos_choice",
           label = "Plot choice",
-          choices =c("Position to Tissue Group",
-                     "Tissue Group to Trait Group",
-                     "Position to Traitgroup")
+          choices =names(circos_choice)
           
         )
       ),
       mainPanel(
         h1("Circos"),
-        tabPanel("Circos Plot",plotOutput("my_circos", width=800, height=800))
+        tabPanel("Circos Plot",
+                 plotOutput("my_circos", width=800, height=800))
       )
     )
   ),
   tabPanel(
-    title="Detailed Top 5 Plots",
+    title="Detailed STAs by Traitgroup",
     sidebarLayout(
       sidebarPanel(
         width=3,
@@ -80,17 +79,13 @@ ui = navbarPage(
         shiny::selectInput(
           inputId = "data_Top5_choice",
           label = "Plot choice",
-          choices =c("Number 1 Trait Group",
-                     "Number 2 Trait Group",
-                     "Number 3 Trait Group",
-                     "Number 4 Trait Group",
-                     "Number 5 Trait Group",
-                     "Other Gene Expression Change")
+          choices = names(Top5_choice)
           
         )
       ),
       mainPanel(
-        tabPanel("Plot",plotOutput("Top5_plot", height=2000, width=600))
+        tabPanel("Plot",
+                 plotOutput("By_traitgroup_plot", height=2000, width=600))
       )
     )
   ),
@@ -144,58 +139,11 @@ server = function(input, output){
   })
   
   
-  output$Top5_plot = renderPlot({
+  output$By_traitgroup_plot = renderPlot({
     g = rv_top5$Top5_choice_by_user
     g
   })
   
-  # my_circos2 = renderPlot({
-  #   circos.clear()
-  #   circos.par( start.degree = 90 ) 
-  #   chordDiagram(test2, 
-  #                annotationTrackHeight = c(0.01, 0.01), 
-  #                annotationTrack = "grid",
-  #                big.gap =15,
-  #                #small.gap = 1,
-  #                column.col = grid_col_trait, # or col_mat
-  #                grid.col = grid_col_trait,
-  #                diffHeight = mm_h(5), 
-  #                # (length(unique(simple_LUT$tissue_group)))
-  #                preAllocateTracks = list(track.height = 0.2*max(strwidth(unlist(dimnames(test3))))),
-  #                transparency = 0.5,
-  #                order = c(rownames(test2), rev(colnames(test2)))
-  #   )
-  #   circos.track(track.index = 1, panel.fun = function(x, y) {
-  #     circos.text(CELL_META$xcenter, CELL_META$ylim[1], CELL_META$sector.index, 
-  #                 facing = "clockwise", niceFacing = TRUE, adj = c(-0.2, 0.5), cex =0.5)
-  #   }, bg.border = NA)
-  # })
-  # 
-  # 
-  # 
-  # 
-  # 
-  # my_circos3 = renderPlot({
-  #   circos.clear()
-  #   circos.par( start.degree = 90 ) 
-  #   chordDiagram(test3, 
-  #                annotationTrackHeight = c(0.01, 0.01), 
-  #                annotationTrack = "grid",
-  #                big.gap =15,
-  #                #small.gap = 1,
-  #                column.col = grid_col_trait, # or col_mat
-  #                grid.col = grid_col_trait,
-  #                diffHeight = mm_h(5), 
-  #                # (length(unique(simple_LUT$tissue_group)))
-  #                preAllocateTracks = list(track.height = 0.2*max(strwidth(unlist(dimnames(test3))))),
-  #                transparency = 0.5,
-  #                order = c(rownames(test3), rev(colnames(test3)))
-  #   )
-  #   circos.track(track.index = 1, panel.fun = function(x, y) {
-  #     circos.text(CELL_META$xcenter, CELL_META$ylim[1], CELL_META$sector.index, 
-  #                 facing = "clockwise", niceFacing = TRUE, adj = c(-0.2, 0.5), cex =0.5)
-  #   }, bg.border = NA)
-  # })
   
 }
 
